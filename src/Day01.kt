0e19d5db2694fda1cb42f062c4ee97f9e91cbe8c
Thomas Introini <thomas.introini@audiencerate.com>
2022-12-01 08:46:10 +0100

    :sparkles: day1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,20 @@
 fun main() {
+
+    fun getWeights(input: List<String>) = input.fold(listOf<Int>()) { acc, s ->
+        if (s.isBlank()) {
+            acc + 0
+        } else {
+            acc.dropLast(1) + ((acc.lastOrNull() ?: 0) + s.toInt())
+        }
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return getWeights(input).max()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return getWeights(input).sortedDescending().take(3).sum()
     }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     println(part1(input))
