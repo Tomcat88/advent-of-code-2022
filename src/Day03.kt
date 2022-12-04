9d4f777ea2271b8b77dce99c8cf92b8f475ab555
Thomas <thomas.introini@gmail.com>
2022-12-04 11:40:40 +0100

    :recycle: polish

--- src/Day03.kt
+++ src/Day03.kt
@@ -4,24 +4,26 @@ fun main() {
         input.sumOf { rucksack ->
             rucksack.chunked(rucksack.length / 2)
                     .map { it.toSet() }
-                    .reduce { acc, e -> acc.intersect(e) }
-                    .firstOrNull()?.toPriority() ?: 0
+                    .reduce { acc, e -> acc intersect e }
+                    .singleOrNull().priority
         }
 
     fun part2(input: List<String>) =
-        input.chunked(3).sumOf {
-            it.map { it.toSet() }
-              .reduce { acc, e -> acc.intersect(e) }
-              .firstOrNull()?.toPriority() ?: 0
+        input.chunked(3).sumOf { rucksack ->
+            rucksack.map { it.toSet() }
+              .reduce { acc, e -> acc intersect e }
+              .singleOrNull().priority
         }
 
     val input = readInput("Day03")
     println(part1(input))
     println(part2(input))
-}
 
-fun Char.toPriority() = if (isUpperCase()) {
-    code - 'A'.code + 27
-} else {
-    code - 'a'.code + 1
 }
+val Char?.priority
+    get() = when(this) {
+       null -> 0
+       in 'a'..'z' -> code - 'a'.code + 1
+       in 'A'..'Z' -> code - 'A'.code + 27
+       else -> error("invalid char: $this")
+    }
--- src/Day04.kt
+++ src/Day04.kt
@@ -1,24 +1,23 @@
 fun main() {
 
-    fun part1(input: List<Pair<String, String>>) =
-       input.map { it.first.toRange().toSet() to it.second.toRange().toSet()}
-            .map {
-                (it.first union it.second).size in listOf(it.second.size, it.first.size)
-            }.count { it }
+    fun part1(input: List<Pair<String, String>>) = input.map { (first, second) ->
+        first.toRangeSet() to second.toRangeSet()
+    }.map { (first, second) ->
+        (first union second).size in listOf(first.size, second.size)
+    }.count { it }
 
-
-    fun part2(input: List<Pair<String, String>>): Int =
-        input.map { it.first.toRange().toSet() to it.second.toRange().toSet() }
-             .map {
-                 (it.first union it.second).size < it.second.size + it.first.size
-             }.count { it }
+    fun part2(input: List<Pair<String, String>>): Int = input.map { (first, second) ->
+        first.toRangeSet() to second.toRangeSet()
+    }.map { (first, second) ->
+        (first union second).size < first.size + second.size
+    }.count { it }
 
     val input = readInputAndSplitInPairs("Day04", ",")
     println(part1(input))
     println(part2(input))
 }
 
-fun String.toRange() = split("-").let {
+fun String.toRangeSet() = split("-").let {
     it[0].toInt()..it[1].toInt()
-}
+}.toSet()
 
