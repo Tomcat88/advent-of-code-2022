d2d947c0e53ea5d7f2ce34ad8c6648631f56dd47
Thomas <thomas.introini@gmail.com>
2022-12-08 20:24:10 +0100

    :recycle: polish

--- src/Day08.kt
+++ src/Day08.kt
@@ -2,10 +2,10 @@
 fun main() {
 
     fun part1(input: List<List<Int>>) {
-        input.withIndex().sumOf { row ->
-            row.value.withIndex().count { col ->
-               row.value.splitOnIndex(col.index).any { it.all { col.value > it } } ||
-               input.column(col.index).splitOnIndex(row.index).any { it.all { col.value > it } }
+        input.withIndex().sumOf { (rowIdx, row) ->
+            row.withIndex().count { (colIdx, col) ->
+               row.splitOnIndex(colIdx).any { it.all { col > it } } ||
+               input.column(colIdx).splitOnIndex(rowIdx).any { it.all { col > it } }
             }
          }.log("part1")
     }
@@ -13,8 +13,8 @@ fun main() {
     fun part2(input: List<List<Int>>) {
         input.flatMapIndexed { rowIdx, row ->
             row.mapIndexed { colIdx, col ->
-                row.splitOnIndex(colIdx, true).map { it.view(col) }.reduce { acc, i -> acc * i } *
-                input.column(colIdx).splitOnIndex(rowIdx, true).map { it.view(col) }.reduce {acc, i -> acc * i  }
+                row.splitOnIndex(colIdx, true).map { it.view(col) }.reduce(Int::times) *
+                input.column(colIdx).splitOnIndex(rowIdx, true).map { it.view(col) }.reduce(Int::times)
             }
         }.max().log("part2")
     }
@@ -25,8 +25,8 @@ fun main() {
     part2(input)
 }
 
-fun List<Int>.splitOnIndex(separator: Int, reverseFirst: Boolean = false) =
-    listOf(subList(0, separator).let { if (reverseFirst) it.reversed() else it }, subList(separator + 1, size))
+fun List<Int>.splitOnIndex(separatorIdx: Int, reverseFirst: Boolean = false) =
+    listOf(subList(0, separatorIdx).let { if (reverseFirst) it.reversed() else it }, subList(separatorIdx + 1, size))
 fun List<List<Int>>.column(i: Int): List<Int> = map { it[i] }
 fun List<Int>.view(v: Int) =
         indexOfFirst { it >= v }.takeIf { it != -1 }?.let { it + 1 } ?: size
